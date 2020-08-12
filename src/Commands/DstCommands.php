abb7c5ad6a93858d3b310dbc04c7b35bc6baa977
vyasamit2007 (Amit Vyas) <vyasamit2007@gmail.com>
2020-08-12 17:06:55 +0530

    DSTEG-00: Created an initial module skeleton.

---
+++ src/Commands/DstCommands.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\dst_entity_generate\Commands;
+
+use Drush\Commands\DrushCommands;
+use Drupal\Core\StringTranslation\TranslationInterface;
+use Consolidation\AnnotatedCommand\CommandResult;
+
+/**
+ * Class DstCommands.
+ *
+ * @package Drupal\dst_entity_generate\Commands
+ */
+class DstCommands extends DrushCommands {
+  use StringTranslationTrait;
+
+  /**
+   * DstCommands constructor.
+   *
+   * @param \Drupal\Core\StringTranslation\TranslationInterface $stringTranslation
+   */
+  public function __construct(TranslationInterface $stringTranslation) {
+    parent::__construct();
+    $this->stringTranslation = $stringTranslation;
+  }
+
+  /**
+   * Generate all the Drupal entities from Drupal Spec tool sheet.
+   *
+   * @command dst:generate
+   * @aliases dst:generate:all dst:ga
+   * @usage drush dst:generate
+   */
+  public function generate() {
+    $this->say($this->t('Generating Drupal entities.'));
+    // Call all the methods to generate the Drupal entities.
+    $this->yell($this->t('Congratulations. All the Drupal entities are generated automatically.'));
+
+    return CommandResult::exitCode(self::EXIT_SUCCESS);
+  }
+
+}

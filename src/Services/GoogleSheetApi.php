effd60889e268a2b36cf328aadb9ee5115bb47fa
ashutosh.gupta <ashutosh.gupta@acquia.com>
2020-08-28 11:50:28 +0530

    DSTEG-01: Code improvements from PR 3.

--- src/Services/GoogleSheetApi.php
+++ src/Services/GoogleSheetApi.php
@@ -5,9 +5,9 @@ namespace Drupal\dst_entity_generate\Services;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 /**
- * Class GoogleSpreadsheetAccess.
+ * Class GoogleSheetApi.
  */
-class GoogleSheetAccess {
+class GoogleSheetApi {
   /**
    * Google client object.
    *
@@ -21,7 +21,7 @@ class GoogleSheetAccess {
    */
   protected $logger;
   /**
-   * Drupal\Core\Config\ConfigFactoryInterface definition.
+   * Drupal\Core\KeyValueStore\KeyValueFactoryInterface definition.
    *
    * @var \Drupal\Core\KeyValueStore\KeyValueFactoryInterface
    */
@@ -48,26 +48,26 @@ class GoogleSheetAccess {
    */
    public function getClient() {
      try {
-       $client = new \Google_Client();
-       $client->setApplicationName($this->keyValue->get('name'));
-       $client->setScopes(\Google_Service_Sheets::SPREADSHEETS_READONLY);
-       $client->setAuthConfig(json_decode($this->keyValue->get('credentials'), TRUE));
-       $client->setAccessType('offline');
+       $google_client = new \Google_Client();
+       $google_client->setApplicationName($this->keyValue->get('name'));
+       $google_client->setScopes(\Google_Service_Sheets::SPREADSHEETS_READONLY);
+       $google_client->setAuthConfig(json_decode($this->keyValue->get('credentials'), TRUE));
+       $google_client->setAccessType('offline');
 
        // Load previously authorized credentials from a file.
        $access_token = json_decode($this->keyValue->get('access_token'), TRUE);
-       $client->setAccessToken($access_token);
+       $google_client->setAccessToken($access_token);
 
        // Refresh the token if it's expired.
-       if ($client->isAccessTokenExpired()) {
-         $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+       if ($google_client->isAccessTokenExpired()) {
+         $google_client->fetchAccessTokenWithRefreshToken($google_client->getRefreshToken());
          $settings = $this->keyValue;
          if (!empty($settings)) {
-           $settings->set('access_token', json_encode($client->getAccessToken()));
+           $settings->set('access_token', json_encode($google_client->getAccessToken()));
          }
        }
-       $this->logger->info('Google Client ceated successfully.');
-       return $client;
+       $this->logger->info('Google Client created successfully.');
+       return $google_client;
      } catch (\Exception $e) {
        // Log the access error of google spreadsheet.
        $this->logger->error('Error creating Google Client @error',[
@@ -87,10 +87,39 @@ class GoogleSheetAccess {
    *   whole sheet.
    */
   public function getData($range) {
-    $service = new \Google_Service_Sheets($this->client);
-    $response = $service->spreadsheets_values->get($this->keyValue->get('spreadsheet'), $range);
-    $this->logger->info('Data fetched successfully.');
-    return $response->getValues();
+    $sheet_values = [];
+    try {
+      $google_sheets_service = new \Google_Service_Sheets($this->client);
+      $response = $google_sheets_service->spreadsheets_values->get($this->keyValue->get('spreadsheet'), $range);
+      if (!empty($response)) {
+        $this->logger->info('Data fetched successfully.');
+        $sheet_values = $response->getValues();
+        $headers = $sheet_values[0];
+
+        // Get header rows. Assuming first row is header row.
+        array_splice($sheet_values, 0, 1);
+
+        // Replacing indexes with header values as keys.
+        foreach ($sheet_values as $key => $value) {
+          $new_sheet_value = [];
+          foreach ($headers as $header_key => $header_value) {
+            $new_sheet_value[
+            preg_replace('/\s+/', '_', strtolower($header_value))
+            ] = $value[$header_key];
+          }
+          $sheet_values[$key] = $new_sheet_value;
+        }
+      }
+      else {
+        $this->logger->notice('Response is empty');
+      }
+    }
+    catch (\Exception $exception) {
+      $this->logger->notice('Error in fetching data from Spec Tool Sheet @exception', [
+        '@exception' => $exception
+      ]);
+    }
+    return $sheet_values;
   }
 
 }

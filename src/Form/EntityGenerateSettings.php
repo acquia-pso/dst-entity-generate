2b4f72bb8ba2c20f99c1b5a57798b8a6b9fcec34
Omkar Pednekar <omkar.pednekar27@gmail.com>
2020-12-18 12:36:57 +0530

    DSTEG-56: Provided all list of entities/fields available for syncing. (#57)
    * DSTEG-56: Provided all list of entities/fields available for syncing.
    
    * DSTEG-56: General settings form corrections.
    
    * DSTEG-56: Removing unwandted dependency.

--- src/Form/EntityGenerateSettings.php
+++ src/Form/EntityGenerateSettings.php
@@ -90,35 +90,14 @@ final class EntityGenerateSettings extends ConfigFormBase {
     if (is_array($entity_list_items) && !empty($entity_list_items)) {
       // Get sync entities from configuration.
       $sync_entities = $config->get('sync_entities');
-
-      // Create fieldset for each entity type.
-      foreach ($entity_list_items as $entity_key => $entity_item) {
-        $form[$entity_key] = [
-          '#type' => 'details',
-          '#title' => $entity_item['label'],
-          '#collapsible' => TRUE,
-          '#collapsed' => TRUE,
-        ];
-
-        $form[$entity_key]['sync_entities_' . $entity_key] = [
-          '#type' => 'checkboxes',
-          '#title' => $this->t('Select entity types'),
-          '#options' => $entity_item['types'],
-          '#default_value' => isset($sync_entities[$entity_key]) ? $sync_entities[$entity_key] : [],
-          '#description' => $this->t('Select @type to sync from the Drupal spec tool sheet.', [
-            '@type' => $entity_item['label'],
-          ]),
-        ];
-      }
+      $form['sync_entities'] = [
+        '#type' => 'checkboxes',
+        '#title' => $this->t('Select Entity Types'),
+        '#options' => $entity_list_items,
+        '#default_value' => isset($sync_entities) ? $sync_entities : [],
+        '#description' => $this->t('Select entity type to sync from the Drupal spec tool sheet.'),
+      ];
     }
-
-    $store = $this->keyValue->get('dst_entity_generate_storage');
-    $form['debug_mode'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Enable debug mode'),
-      '#default_value' => isset($store) ? $store->get('debug_mode') : FALSE,
-      '#description' => $this->t('Check this box to see connection messages. Keep it disabled in live environments.'),
-    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -129,25 +108,11 @@ final class EntityGenerateSettings extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
     $form_values = $form_state->getValues();
-    $sync_entity_list = [];
-    foreach ($form_values as $entity_key => $entity_types) {
-      if (str_starts_with($entity_key, 'sync_entities_')) {
-        // Remove prefix before save.
-        $entity_name = str_replace('sync_entities_', '', $entity_key);
-        foreach ($entity_types as $type_key => $type_value) {
-          // Make list to be saved on config.
-          $sync_entity_list[$entity_name][$type_key] = $type_value;
-        }
-      }
-    }
 
     // Save sync entities in config.
     $this->config(self::SETTINGS)
-      ->set('sync_entities', $sync_entity_list)
+      ->set('sync_entities', $form_values['sync_entities'])
       ->save();
-
-    $store = $this->keyValue->get("dst_entity_generate_storage");
-    $store->set('debug_mode', $form_state->getValue('debug_mode'));
   }
 
   /**
@@ -157,24 +122,27 @@ final class EntityGenerateSettings extends ConfigFormBase {
    *   Entity List of entities from sheet.
    */
   private function getEntityList() {
-    $dst_entity_group = '';
     $entity_list = [];
+    $skip_entity_listing = [
+      'Bundles',
+      'Fields',
+      'Workflow states',
+      'Workflow transitions',
+    ];
     $overview_records = $this->googleSheetApi->getData(DstegConstants::OVERVIEW);
     if (isset($overview_records) && !empty($overview_records)) {
       foreach ($overview_records as $overview) {
-        $clean_spec = trim($overview['specification']);
-        if (!empty($clean_spec)) {
-          if (str_starts_with($clean_spec, '-')) {
-            $clean_spec = trim($clean_spec, '- ');
-            $entity_list[$dst_entity_group]['types'][$clean_spec] = $clean_spec;
+        if ($overview['total'] > 0) {
+          $clean_spec = trim($overview['specification']);
+          if (in_array($clean_spec, $skip_entity_listing)) {
+            continue;
           }
-          else {
+          if (!empty($clean_spec)) {
+            if (str_starts_with($clean_spec, '-')) {
+              $clean_spec = trim($clean_spec, '- ');
+            }
             $lower_clean_spec = preg_replace('/\s+/', '_', strtolower($clean_spec));
-            $entity_list[$lower_clean_spec] = [
-              'label' => $clean_spec,
-              'types' => ['All' => 'All'],
-            ];
-            $dst_entity_group = $lower_clean_spec;
+            $entity_list[$lower_clean_spec] = $clean_spec;
           }
         }
       }

8aa743ab3044edb12a834db376add625593aa84d
Gaurav Goyal <gaurav.goyal@acquia.com>
2021-01-08 11:57:02 +0530

    Fixed Generate all command refactoring.

--- src/Commands/GenerateAll.php
+++ src/Commands/GenerateAll.php
@@ -3,152 +3,14 @@
 namespace Drupal\dst_entity_generate\Commands;
 
 use Consolidation\AnnotatedCommand\CommandResult;
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\StringTranslation\TranslationInterface;
-use Drupal\dst_entity_generate\BaseEntityGenerate;
-use Drupal\dst_entity_generate\Services\GeneralApi;
-use Drupal\dst_entity_generate\Services\GoogleSheetApi;
+use Drush\Commands\DrushCommands;
 
 /**
  * Class provides functionality of supported entity generation from DST sheet.
  *
  * @package Drupal\dst_entity_generate\Commands
  */
-class GenerateAll extends BaseEntityGenerate {
-
-  /**
-   * Bundle command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\Bundle
-   */
-  protected $bundle;
-
-  /**
-   * Menu command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\Menu
-   */
-  protected $menus;
-
-  /**
-   * UserRole command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\UserRole
-   */
-  protected $userRoles;
-
-  /**
-   * ImageEffect command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\ImageEffect
-   */
-  protected $imageEffect;
-
-  /**
-   * Workflow command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\Workflow
-   */
-  protected $workflow;
-
-  /**
-   * ImageStyle command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\ImageStyle
-   */
-  protected $imageStyle;
-
-  /**
-   * DstegVocabulary command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\Vocabulary
-   */
-  protected $vocabulary;
-
-  /**
-   * Generate media command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\Media
-   */
-  protected $media;
-
-  /**
-   * Generate paragraph command definition.
-   *
-   * @var \Drupal\dst_entity_generate\Commands\Paragraph
-   */
-  protected $paragraph;
-
-  /**
-   * {@inheritDoc}
-   */
-  protected $dstEntityName = 'all';
-
-  /**
-   * {@inheritdoc}
-   */
-  protected $dependentModules = [
-    'workflows',
-    'content_moderation',
-    'media',
-    'paragraphs',
-  ];
-
-  /**
-   * DstCommands constructor.
-   *
-   * @param \Drupal\Core\StringTranslation\TranslationInterface $stringTranslation
-   *   StringTranslation service definition.
-   * @param Bundle $dstegBundle
-   *   Bundle command definition.
-   * @param Menu $dstegMenus
-   *   DstegMenus command definition.
-   * @param UserRole $dstegUserRoles
-   *   DstegUserRoles command definition.
-   * @param ImageEffect $dstegImageEffect
-   *   ImageEffect command definition.
-   * @param Workflow $dstegWorkflows
-   *   DstegWorkflows command definition.
-   * @param ImageStyle $dstegImageStyle
-   *   DstegImageStyle command definition.
-   * @param Vocabulary $dstegVocabulary
-   *   Vocabulary command definition.
-   * @param \Drupal\dst_entity_generate\Services\GoogleSheetApi $sheet
-   *   GoogleSheetApi service class object.
-   * @param \Drupal\dst_entity_generate\Services\GeneralApi $generalApi
-   *   The helper service for DSTEG.
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
-   *   The config factory.
-   * @param \Drupal\dst_entity_generate\Commands\Media $media
-   *   Media generate command definition.
-   * @param \Drupal\dst_entity_generate\Commands\Paragraph $paragraph
-   *   Paragraph generate command definition.
-   */
-  public function __construct(TranslationInterface $stringTranslation,
-                              Bundle $dstegBundle,
-                              Menu $dstegMenus,
-                              UserRole $dstegUserRoles,
-                              ImageEffect $dstegImageEffect,
-                              Workflow $dstegWorkflows,
-                              ImageStyle $dstegImageStyle,
-                              Vocabulary $dstegVocabulary,
-                              GoogleSheetApi $sheet,
-                              GeneralApi $generalApi,
-                              ConfigFactoryInterface $configFactory,
-                              Media $media,
-                              Paragraph $paragraph) {
-    parent::__construct($sheet, $generalApi, $configFactory);
-    $this->stringTranslation = $stringTranslation;
-    $this->bundle = $dstegBundle;
-    $this->menus = $dstegMenus;
-    $this->userRoles = $dstegUserRoles;
-    $this->imageEffect = $dstegImageEffect;
-    $this->workflow = $dstegWorkflows;
-    $this->imageStyle = $dstegImageStyle;
-    $this->vocabulary = $dstegVocabulary;
-    $this->media = $media;
-    $this->paragraph = $paragraph;
-  }
+class GenerateAll extends DrushCommands {
 
   /**
    * Generate all the Drupal entities from Drupal Spec tool sheet.
@@ -158,39 +20,35 @@ class GenerateAll extends BaseEntityGenerate {
    * @usage drush dst:generate
    */
   public function generate() {
-    $this->say($this->t('Generating Drupal entities.'));
+    $this->io()->success('Generating All Drupal entities.');
 
+    // @todo Further refactor it so that we don't have to use exec fuynction.
     // Generate Menus.
-    $this->menus->generateMenus();
+    \exec('drush dst:m');
 
-    // Generate User roles.
-    $this->userRoles->generateUserRoles();
+    // Generate User Roles.
+    \exec('drush dst:ur');
 
-    // Generate image styles.
-    $this->imageStyle->generateImageStyle();
+    // Generate Image Styles.
+    \exec('drush dst:is');
 
-    // Generate Image effects.
-    $this->imageEffect->generateImageEffects();
+    // Generate Image Effects.
+    \exec('drush dst:ie');
 
-    // Generate workflows.
-    $this->workflow->generateWorkflows();
+    // Generate Workflow.
+    \exec('drush dst:w');
 
-    // Generate vocabularies.
-    $this->vocabulary->generateVocabularies();
+    // Generate Vocabularies.
+    \exec('drush dst:v');
 
-    // Generate media types.
-    $this->media->generateBundle();
+    // Generate Media.
+    \exec('drush dst:media');
 
-    // Generate bundles.
-    $this->bundle->generateBundle();
+    // Generate Paragraphs.
+    \exec('drush dst:p');
 
-    // Generate paragraphs.
-    $this->paragraph->generateParagraph();
-
-    // Call all the methods to generate the Drupal entities.
-    $this->io()->success('Congratulations. The entities which are enabled for sync are generated successfully.');
-
-    return CommandResult::exitCode(self::EXIT_SUCCESS);
+    // Generate Bundles.
+    \exec('drush dst:b');
   }
 
 }
